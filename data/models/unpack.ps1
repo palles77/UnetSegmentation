0e823225e6915f9fcd2327cd6dcb1f3983e246a5
Lesław Pawlaczyk <leslaw.pawlaczyk@recogify.com>
2025-05-29 22:48:32 +0200

    Adding temporary models

--- data/models/unpack.ps1
+++ data/models/unpack.ps1
@@ -2,7 +2,9 @@ $models = @(
     "cnn_model_20250214_222252",
     "unet_model_20250212_002522",
     "cnn_model_20250214_222252.zip",
-    "unet_model_20250224_183427.zip"
+    "unet_model_20250224_183427.zip",
+    "cnn_model_20250529_005118.zip",
+    "unet_model_20250529_000149.zip"
 )
 
 foreach ($model in $models) {
